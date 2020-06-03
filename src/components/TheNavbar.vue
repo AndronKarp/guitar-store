a147c341bbf2dd62068db67a2117c91c817e25ff
Andrew <andrewkarp00@gmail.com>
2020-06-03 23:45:26 +0300

    added TheNavbar component

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar type="dark" variant="info">
+    <b-navbar-brand to="/">Guitar Store</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item>Contact Us!</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>
+            <b-avatar variant="primary" text="JD"></b-avatar>
+          </template>
+          <b-dropdown-item>Profile</b-dropdown-item>
+          <b-dropdown-item>Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
