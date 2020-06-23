85ac126af2924c45a0d8fd0d347be56461c71b40
Andrew <andrewkarp00@gmail.com>
2020-06-23 21:12:21 +0300

    added tests for guitar description page

---
+++ src/tests/unit/pages/page-guitar-description.spec.js
@@ -0,0 +1,76 @@
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import Vuex from "vuex";
+import guitars from "@/store/modules/guitars";
+import PageGuitarDescription from "@/pages/PageGuitarDescription";
+import BootstrapVue from "bootstrap-vue";
+
+const localVue = createLocalVue();
+
+localVue.use(BootstrapVue);
+localVue.use(Vuex);
+
+describe("PageGuitarDescription.vue", () => {
+  let wrapper;
+  let store;
+  let guitar;
+  let state;
+  let actions;
+
+  beforeEach(() => {
+    guitar = {
+      brand: "Gibson",
+      model: "SG",
+      description: "Guitar",
+      quantity: 1,
+      slug: "sg"
+    };
+    state = { guitars: [guitar] };
+    actions = {
+      updateGuitarQuantity: jest.fn(),
+      addToCart: jest.fn()
+    };
+    store = new Vuex.Store({
+      state,
+      getters: guitars.getters,
+      actions
+    });
+    wrapper = shallowMount(PageGuitarDescription, {
+      localVue,
+      store,
+      propsData: {
+        slug: "sg"
+      }
+    });
+  });
+
+  test("renders guitar description", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe("computed", () => {
+    test("guitar returns guitars item finded by its slug", () => {
+      expect(wrapper.vm.guitar).toEqual(guitar);
+    });
+
+    describe("isGuitarInStock", () => {
+      test("returns true if quantity property of guitar is greater than zero", () => {
+        expect(wrapper.vm.isGuitarInStock(guitar)).toBe(true);
+      });
+
+      test("returns false if quantiy property of guitar is equal to zero", () => {
+        guitar.quantity = 0;
+        expect(wrapper.vm.isGuitarInStock(guitar)).toBe(false);
+      });
+    });
+  });
+
+  describe("methods", () => {
+    describe("addToCart", () => {
+      test("calls addToCart and updateGuitarQuantity actions", () => {
+        wrapper.vm.addToCart(guitar);
+        expect(actions.addToCart).toHaveBeenCalled();
+        expect(actions.updateGuitarQuantity).toHaveBeenCalled();
+      });
+    });
+  });
+});
