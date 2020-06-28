f232635894a92a02cb7e8ee4aa71d391625de6ca
Andrew <andrewkarp00@gmail.com>
2020-06-28 15:34:58 +0300

    added tests for App.vue

---
+++ src/tests/unit/app.spec.js
@@ -0,0 +1,50 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import App from "@/App";
+import Vuex from "vuex";
+import { BootstrapVue } from "bootstrap-vue";
+import guitars from "@/store/modules/guitars";
+import TheNavbar from "@/components/TheNavbar";
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+localVue.use(BootstrapVue);
+
+describe("App.vue", () => {
+  let state;
+  let actions;
+  let store;
+  let wrapper;
+
+  beforeEach(() => {
+    state = { areGuitarsFetched: true };
+    actions = {
+      fetchGuitars: jest.fn()
+    };
+    store = new Vuex.Store({
+      state,
+      getters: guitars.getters,
+      actions
+    });
+    wrapper = shallowMount(App, {
+      localVue,
+      store,
+      stubs: ["router-view"]
+    });
+  });
+
+  test("renders app", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("accepts TheNavbar child component", () => {
+    const theNavbar = wrapper.findComponent(TheNavbar);
+    expect(theNavbar.exists()).toBe(true);
+  });
+
+  describe("lifecycle methods", () => {
+    test("created calls fetchGuitars vuex action", () => {
+      expect(actions.fetchGuitars).toHaveBeenCalled();
+    });
+  });
+});
