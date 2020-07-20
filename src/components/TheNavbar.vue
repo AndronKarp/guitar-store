f8b11d72312cb4f2b9d03086f42b10c0a462ffc8
Andrew <andrewkarp00@gmail.com>
2020-07-20 23:29:18 +0300

    added conditional render of navbar items

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -11,9 +11,24 @@
         <b-nav-item>Contact Us!</b-nav-item>
       </b-navbar-nav>
 
-      <b-navbar-nav class="ml-auto">
+      <b-navbar-nav v-if="currentUser" class="ml-auto">
+        <b-nav-item to="/cart">
+          <b-icon-cart4></b-icon-cart4>
+        </b-nav-item>
+        <b-dropdown :text="currentUser.displayName" variant="info">
+          <b-dropdown-item>Profile</b-dropdown-item>
+          <b-dropdown-item @click="signOut">Sign Out</b-dropdown-item>
+        </b-dropdown>
+      </b-navbar-nav>
+
+      <b-navbar-nav v-else class="ml-auto">
         <b-nav-item>
-          <b-button to="/registration" variant="info" right>Sign Up</b-button>
+          <b-button to="/registration" variant="info">Sign Up</b-button>
+        </b-nav-item>
+        <b-nav-item>
+          <b-button to="/authorization" variant="outline-light"
+            >Sign In</b-button
+          >
         </b-nav-item>
       </b-navbar-nav>
     </b-collapse>
@@ -21,7 +36,20 @@
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+import { auth } from "../configs/firebase";
+
+export default {
+  computed: {
+    ...mapGetters(["currentUser"])
+  },
+  methods: {
+    signOut() {
+      auth().signOut();
+      this.$router.push("/authorization");
+    }
+  }
+};
 </script>
 
 <style></style>
