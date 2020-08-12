4f0a1a197bcf2ef9a987bbf472a195d9623e3c3f
Andrew <andrewkarp00@gmail.com>
2020-08-12 20:47:56 +0300

    temporarily removed cart is empty message

--- src/pages/PageShoppingCart.vue
+++ src/pages/PageShoppingCart.vue
@@ -1,20 +1,11 @@
 <template>
-  <div class="w-100">
-    <ShoppingCart v-show="!isCartEmpty" />
-    <b-alert :show="isCartEmpty" variant="info">
-      <p class="mb-0">The cart is empty for now!</p>
-    </b-alert>
-  </div>
+  <ShoppingCart />
 </template>
 
 <script>
 import ShoppingCart from "../components/ShoppingCart";
-import { mapGetters } from "vuex";
 
 export default {
-  computed: {
-    ...mapGetters(["isCartEmpty"])
-  },
   components: {
     ShoppingCart
   }
